043806ac351a76646b6c443d346798842698579c
GLYB-85 <gou1377877@126.com>
2022-08-09 21:31:04 +0800

    完成了主页功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ $(function () {
     getUserInfo()
 
     let layer = layui.layer
-    // 给退出按钮绑定退出事件 layui内置模块
+    // 点击按钮 退出 layui内置模块
     $('#btnLogout').on('click', function () {
         layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function (index) {
 
@@ -26,7 +26,8 @@ function getUserInfo() {
         url: 'my/userinfo',
         // 以/my 开头的API接口有请求权限
         // 在请求头中添加Authorization属性和对应的token值
-        // token值在locostorage中取
+        // token值在locostorage中取 
+        // 全局挂载
         /*  headers: {
              Authorization: localStorage.getItem('token') || ''
          }, */
@@ -37,7 +38,25 @@ function getUserInfo() {
 
             // 调用 renderAvatar 渲染用户的头像
             renderAvatar(res.data)
-        }
+        },
+
+        // 不论成功还是失败，最终都会调用 complete 回调函数
+        // 全局挂载
+        /* complete: function (res) {
+
+            // console.log('执行了 complete回调函数')
+            console.log(res)
+            // 在complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+
+            if (res.responseJSON.status === 1 && res.responseJSON.message == '身份认证失败！') {
+                // 1.强制清空 token
+                localStorage.removeItem('token')
+
+                // 2.强制跳转到登录页面
+                location.href='./login.html'
+            }
+
+        } */
     })
 }
 
